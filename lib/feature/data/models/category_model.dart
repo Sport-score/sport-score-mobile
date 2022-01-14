812e35e774e831d962ea9dacbbd8b600d0debff0
Ivan Lesnykh <ivan.lesnykh.official@gmail.com>
2022-01-15 00:56:48 +0300

    Delete from category entity events field.

--- lib/feature/data/models/category_model.dart
+++ lib/feature/data/models/category_model.dart
@@ -1,5 +1,4 @@
 import 'package:meta/meta.dart';
-import 'package:sport_shedule_mobile/feature/data/models/event_model.dart';
 import 'package:sport_shedule_mobile/feature/domain/entities/category_entity.dart';
 
 class CategoryModel extends CategoryEntity{
@@ -7,18 +6,22 @@ class CategoryModel extends CategoryEntity{
   CategoryModel({
     @required id,
     @required name,
-    @required events,
   }): super(
     id: id,
     name: name,
-    events: events,
   );
 
   factory CategoryModel.fromJson(Map<String, dynamic> json){
     return CategoryModel(
       id: json['id'],
       name: json['name'],
-      events: (json['events'] as List<dynamic>).map((event) => event != null ? EventModel.fromJson(event) : null as EventModel).toList(),
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+    };
   }
 }
--- lib/feature/domain/entities/category_entity.dart
+++ lib/feature/domain/entities/category_entity.dart
@@ -1,19 +1,14 @@
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
-
-import 'package:sport_shedule_mobile/feature/domain/entities/event_entity.dart';
 
 class CategoryEntity extends Equatable {
   final int id;
   final String name;
-  final List<EventEntity> events;
 
-  CategoryEntity({
+  const CategoryEntity({
     required this.id,
     required this.name,
-    required this.events
   });
 
   @override
-  List<Object> get props => [id, name, events];
+  List<Object> get props => [id, name];
 }
